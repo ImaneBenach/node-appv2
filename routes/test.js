0a2e5caf8e77882044e7b4ca6d354b28abc1d1bb
Imane <imane@MacBook-Pro-de-Imane.local>
2020-03-20 15:18:47 +0100

    test

--- routes/test.js
+++ routes/test.js
@@ -1,7 +1,8 @@
 const test = require('ava');
 const http = require('http');
+const fetch = require('node-fetch');
 
-function request(url) {
+function httpGet(url) {
   return new Promise((resolve, reject) => {
     http.get(url, (res) => {
       res.on('data', (buffer) => {
@@ -12,10 +13,29 @@ function request(url) {
     });
   });
 }
+
+function httpPost(url, body) {
+  return fetch(url, {
+          method: 'post',
+          body:    JSON.stringify(body),
+          headers: { 'Content-Type': 'application/json' },
+      })
+      .then(res => res.text());
+}
  
 test('home page should contain a form', async t => {
   // condition de passage: la page contient un formulaire HTML
   // plus concretement: la reponse à HTTP `GET /` doit contenir `<form`
-  const html = await request('http://localhost:3000/');
+  const html = await httpGet('http://localhost:3000/');
   t.regex(html, /<form/);
+});
+
+test('city page should contain the name of the city', async t => {
+  const html = await httpPost('http://localhost:3000/ville', { ville: 'paris' });
+  t.regex(html, /paris/);
+});
+
+test('city name with accentuated character should be supported', async t => {
+  const html = await httpPost('http://localhost:3000/ville', { ville: 'nîmes' });
+  t.regex(html, /nîmes/);
 });
