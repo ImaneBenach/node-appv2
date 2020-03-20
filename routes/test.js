68162db9d0f5150f6e41e489e376ff945eaac321
Imane <imane@MacBook-Pro-de-Imane.local>
2020-03-20 12:32:47 +0100

    test

--- routes/test.js
+++ routes/test.js
@@ -1,10 +1,21 @@
 const test = require('ava');
+const http = require('http');
+
+function request(url) {
+  return new Promise((resolve, reject) => {
+    http.get(url, (res) => {
+      res.on('data', (buffer) => {
+        resolve(buffer.toString('utf8'));
+      });  
+    }).on('error', (e) => {
+      reject(e);
+    });
+  });
+}
  
-test('foo', t => {
-    t.pass();
-});
- 
-test('bar', async t => {
-    const bar = Promise.resolve('bar');
-    t.is(await bar, 'bar');
+test('home page should contain a form', async t => {
+  // condition de passage: la page contient un formulaire HTML
+  // plus concretement: la reponse à HTTP `GET /` doit contenir `<form`
+  const html = await request('http://localhost:3000/');
+  t.regex(html, /<form/);
 });
